7e65a9ed80a4f24dbde6ac5fa568c9c9755914e9
mxs2361 <mxs2361@cse-133725-login.uta.edu>
2022-09-22 02:17:31 -0400

    train pipeline works

---
+++ train.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from encoder import Encoder
+from decoder import Decoder
+from seq2seq import Seq2Seq
+from dna_seq_dataset import DNASeqDataset
+
+from tqdm import tqdm
+import time
+import math 
+
+INPUT_DIM = 691#len(SRC.vocab)
+OUTPUT_DIM = 5 #len(TRG.vocab)
+EMB_DIM = 256
+HID_DIM = 512 # each conv. layer has 2 * hid_dim filters
+ENC_LAYERS = 10 # number of conv. blocks in encoder
+DEC_LAYERS = 10 # number of conv. blocks in decoder
+ENC_KERNEL_SIZE = 3 # must be odd!
+DEC_KERNEL_SIZE = 3 # can be even or odd
+ENC_DROPOUT = 0.25
+DEC_DROPOUT = 0.25
+TRG_PAD_IDX = 0
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+enc = Encoder(INPUT_DIM, EMB_DIM, HID_DIM, ENC_LAYERS, ENC_KERNEL_SIZE, ENC_DROPOUT, device)
+dec = Decoder(OUTPUT_DIM, EMB_DIM, HID_DIM, DEC_LAYERS, DEC_KERNEL_SIZE, DEC_DROPOUT, TRG_PAD_IDX, device)
+
+model = Seq2Seq(enc, dec).to(device)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
+
+
+dna_seq_dataset = DNASeqDataset(device=device)
+dna_seq_dataset.prepare_data()
+trainloader = dna_seq_dataset.train_dataloader()
+valloader = dna_seq_dataset.val_dataloader()
+
+
+def train(model, iterator, optimizer, criterion, clip):
+    
+    model.train()
+    
+    epoch_loss = 0
+    
+    for i, batch in tqdm(enumerate(iterator)):
+        
+        src, trg= batch
+        # print(src.size(), trg.size())
+        
+        optimizer.zero_grad()
+        
+        output, _ = model(src, trg[:,:-1])
+        
+        #output = [batch size, trg len - 1, output dim]
+        #trg = [batch size, trg len]
+        
+        output_dim = output.shape[-1]
+        
+        output = output.contiguous().view(-1, output_dim)
+        trg = trg[:,1:].contiguous().view(-1)
+        
+        #output = [batch size * trg len - 1, output dim]
+        #trg = [batch size * trg len - 1]
+        
+        loss = criterion(output, trg)
+        
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+def evaluate(model, iterator, criterion):
+    
+    model.eval()
+    
+    epoch_loss = 0
+    
+    with torch.no_grad():
+    
+        for i, batch in enumerate(iterator):
+
+            src, trg= batch
+
+            output, _ = model(src, trg[:,:-1])
+        
+            #output = [batch size, trg len - 1, output dim]
+            #trg = [batch size, trg len]
+
+            output_dim = output.shape[-1]
+            
+            output = output.contiguous().view(-1, output_dim)
+            trg = trg[:,1:].contiguous().view(-1)
+
+            #output = [batch size * trg len - 1, output dim]
+            #trg = [batch size * trg len - 1]
+            
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+        
+    return epoch_loss / len(iterator)
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+N_EPOCHS = 10
+CLIP = 0.1
+
+best_valid_loss = float('inf')
+
+for epoch in tqdm(range(N_EPOCHS)):
+    
+    start_time = time.time()
+    
+    train_loss = train(model, trainloader, optimizer, criterion, CLIP)
+    valid_loss = evaluate(model, valloader, criterion)
+    
+    end_time = time.time()
+    
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut5-model.pt')
+    
+    print(f'Epoch: {epoch+1:02} ')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
